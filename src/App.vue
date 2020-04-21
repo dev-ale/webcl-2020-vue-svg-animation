94c66d38286bf23c552304d5c2052cf1b5c6de22
Alejandro Garcia <alejandro.g@me.com>
2020-04-21 13:08:30 +0200

    kleines Beispiel mit Smile Einzelne Komponenten des SVG's werden mit einem v-if ein oder ausgeblendet Nicht sicher ob das der Sinn der Sache ist :-)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Eyes/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Eyes from './components/Eyes.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Eyes
   }
 }
 </script>
---
+++ src/components/Eyes.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>Hello Eyes</p>
+        <div>
+
+            <svg width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle @click="toggleHappy" cx="100" cy="100" r="100" :fill="color"/>
+                <circle cx="62" cy="50" r="15" fill="white"/>
+                <circle cx="135" cy="50" r="15" fill="white"/>
+                <line v-if="this.happyStatus" x1="30" y1="123.5" x2="170" y2="123.5" stroke="white" stroke-width="7"/>
+                <path v-if="!this.happyStatus" d="M30 121.5L46.3813 141.189C52.655 148.73 61.0079 154.258 70.4003 157.086L85.5827 161.658C94.9857 164.49 105.014 164.49 114.417 161.658L129.6 157.086C138.992 154.258 147.345 148.73 153.619 141.189L170 121.5" stroke="white" stroke-width="7"/>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+            data:() => ({
+                color: '#EDD60C',
+                happyStatus: false,
+            }),
+            methods: {
+                toggleHappy() {
+                    this.happyStatus = !this.happyStatus;
+                }
+            }
+    }
+
+</script>
