5d6051839d44d16cbe885e292649bd75d841f454
Alejandro Garcia <alejandro.g@me.com>
2020-04-21 14:39:06 +0200

    Augen bewegen sich nur wenn smily lacht... (v-bind:class)

--- src/components/Eyes.vue
+++ src/components/Eyes.vue
@@ -8,9 +8,10 @@
                 <circle cx="135" cy="50" r="15" fill="white"/>
                 <line v-if="!this.happyStatus" x1="30" y1="123.5" x2="170" y2="123.5" stroke="white" stroke-width="7"/>
                 <path v-if="this.happyStatus" d="M30 121.5L46.3813 141.189C52.655 148.73 61.0079 154.258 70.4003 157.086L85.5827 161.658C94.9857 164.49 105.014 164.49 114.417 161.658L129.6 157.086C138.992 154.258 147.345 148.73 153.619 141.189L170 121.5" stroke="white" stroke-width="7"/>
-                <circle class="moving-eyes" cx="139.5" cy="57.5" r="4.5" fill="black"/>
-                <circle class="moving-eyes" cx="66.5" cy="57.5" r="4.5" fill="black"/>
+                <circle :class="{ 'moving-eyes': happyStatus}" cx="137.5" cy="57.5" r="4.5" fill="black"/>
+                <circle :class="{ 'moving-eyes': happyStatus}" cx="66.5" cy="57.5" r="4.5" fill="black"/>
             </svg>
+
 
         </div>
         <p>{{happyMessage}}</p>
@@ -43,25 +44,22 @@
         width: 15%;
         top: 1%;
         left:5%;
-        animation: lookingAround 2s infinite;
+        animation: lookingAround 4s infinite;
     }
+
     @keyframes lookingAround {
-        0% {    transform: rotateX(20deg)    }
-        10% {    transform: rotateY(15deg)    }
-        20% {    transform: rotateY(20deg)    }
-        30% {    transform: rotateY(15deg)    }
-        40% {   transform: rotateX(20deg)    }
-        50% {   transform: rotateX(25deg)    }
-        60% {   transform: rotateX(30deg)    }
-        70% {    transform: rotateX(35deg)    }
-        80% {    transform: rotateX(40deg)    }
-        90% {   transform: rotateX(30deg)    }
+        0%   {    transform: rotateX(20deg)   }
+        10%  {    transform: rotateY(15deg)   }
+        20%  {    transform: rotateY(20deg)   }
+        30%  {    transform: rotateY(15deg)   }
+        40%  {   transform: rotateX(20deg)    }
+        50%  {   transform: rotateX(25deg)    }
+        60%  {   transform: rotateX(30deg)    }
+        70%  {    transform: rotateX(35deg)   }
+        80%  {    transform: rotateX(40deg)   }
+        90%  {   transform: rotateX(30deg)    }
         100% {   transform: rotateX(25deg)    }
 
     }
 
-    @keyframes rotate{
-        0%{transform: rotate(0deg);}
-        100%{transform: rotate(5deg);}
-    }
 </style>
